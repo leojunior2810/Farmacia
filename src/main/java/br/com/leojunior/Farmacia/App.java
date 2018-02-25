881bcc360bf096362c7cc16ff69f2557d8b6de3f
Leonildo Jr <Leonildo Jr@LAPTOP-D6G1AJDN>
2018-02-24 22:22:53 -0300

    Estrutura inicial do projeto.

---
+++ src/main/java/br/com/leojunior/Farmacia/App.java
@@ -0,0 +1,13 @@
+package br.com.leojunior.Farmacia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
