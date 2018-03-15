db90f6a77d75dcb3d0ce5229ee93a8327be50915
Leonildo Jr <Leonildo Jr@LAPTOP-D6G1AJDN>
2018-03-14 23:01:45 -0300

    Criação das classes de modelo Cliente e Funcionario.

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,132 @@
+package model;
+
+import java.util.Date;
+
+public class Cliente {
+
+	private int idCLIENTE;
+	private String nome;
+	private Date dta_nasc;
+	private String cpf;
+	private String rg;
+	private String tel;
+	private String cel;
+	private String endereco;
+	private int num_casa;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private int cep;
+	private String uf;
+	private char sexo;
+	private String email;
+	private Date dta_cadastro;
+	
+	
+	public int getIdCLIENTE() {
+		return idCLIENTE;
+	}
+	public void setIdCLIENTE(int idCLIENTE) {
+		this.idCLIENTE = idCLIENTE;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDta_nasc() {
+		return dta_nasc;
+	}
+	public void setDta_nasc(Date dta_nasc) {
+		this.dta_nasc = dta_nasc;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getCel() {
+		return cel;
+	}
+	public void setCel(String cel) {
+		this.cel = cel;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getNum_casa() {
+		return num_casa;
+	}
+	public void setNum_casa(int num_casa) {
+		this.num_casa = num_casa;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public int getCep() {
+		return cep;
+	}
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDta_cadastro() {
+		return dta_cadastro;
+	}
+	public void setDta_cadastro(Date dta_cadastro) {
+		this.dta_cadastro = dta_cadastro;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/model/Funcionario.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Funcionario extends Cliente {
+
+	public Funcionario(String nome, String cpf, String rg, String endereco, int num_casa, String complemento, String bairro, String cidade, String uf, String email, char sexo) {
+		super();
+		
+	}
+
+	
+	
+	
+	
+}
